18fa384a14a59333988359636c885939348f650f
Adam <riversa@quadigital.co.uk>
2013-07-29 23:25:35 +0100

    Database config finished and commented

--- src/library/Quadigital/Database/Connector/DatabaseConfig.php
+++ src/library/Quadigital/Database/Connector/DatabaseConfig.php
@@ -1,28 +1,47 @@
 <?php
 /**
- * File description.
+ * This file contains the database configuration class that simply holds and
+ * validates the database configuration options.
  *
  * @author    Adam Rivers <riversa@quadigital.co.uk>
  * @copyright 2012-2013 Quadigital <contact@quadigital.co.uk>
  * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
  * PHP version 5
  */
-
 namespace Quadigital\Database\Connector;
-
 
 use Quadigital\Database\Exception\DatabaseException;
 
+/**
+ * Class DatabaseConfig
+ *
+ * @package Quadigital\Database\Connector
+ */
 class DatabaseConfig
 {
 
+    /**
+     * @var array Array containing the database configuration.
+     */
     private $_config = array();
 
+    /**
+     * Database configuration constructor. Sets the database configuration using the elements in the array.
+     * Valid keys: 'database', 'host', 'options', 'password', 'port', 'rdbms', 'username'.
+     *
+     * @param array $config Array containing the database configuration.
+     */
     public function __construct(array $config = array())
     {
         $this->setFromArray($config);
     }
 
+    /**
+     * Set database configuration using elements in the array.
+     * Valid keys: 'database', 'host', 'options', 'password', 'port', 'rdbms', 'username'.
+     *
+     * @param array $config Array containing the database configuration.
+     */
     public function setFromArray(array $config)
     {
         $validKeys = array('database', 'host', 'options', 'password', 'port', 'rdbms', 'username');
@@ -34,7 +53,17 @@ class DatabaseConfig
         }
     }
 
-    private function _exists($key, $config = null) {
+    /**
+     * Checks to see if an element is present in an array.
+     *
+     * @param string     $key    The key of the element to check array for.
+     * @param null|array $config The array to search. If null then it will check the config
+     *                           property. By default this is null.
+     *
+     * @return bool True if the key exists in the array.
+     */
+    private function _exists($key, $config = null)
+    {
         if ($config === null) {
             return isset($this->_config[$key]);
         }
@@ -42,7 +71,16 @@ class DatabaseConfig
         return isset($config[$key]);
     }
 
-    private function _isString($key, $trim = true) {
+    /**
+     * Check if an element in an array is a string. Also allows the option to trim the string.
+     *
+     * @param string $key  The key of the element to check value for.
+     * @param bool   $trim Trim the value if it is a string? By default this is true.
+     *
+     * @return bool True if the value for the specified specified element is a string.
+     */
+    private function _isString($key, $trim = true)
+    {
         $isString = false;
         $value = &$this->_config[$key];
 
@@ -57,11 +95,21 @@ class DatabaseConfig
         return $isString;
     }
 
-    private function _isAlpha($key, $trim = true) {
+    /**
+     * Check if an element in an array contains only alphabetical characters. Also allows the option to trim the string.
+     *
+     * @param string $key  The key of the element to check value for.
+     * @param bool   $trim Trim the value if it is alphabetic? By default this is true.
+     *
+     * @return bool True if the value of the specified element contains only alphabetical characters.
+     */
+    private function _isAlpha($key, $trim = true)
+    {
         $isAlpha = false;
         $value = &$this->_config[$key];
 
-        if ($this->_isString($key, $trim) && ctype_alpha($value)) {
+        // If trim is enabled then execute '_isString()' first so any whitespace can be trimmed.
+        if ((!$trim || $this->_isString($key, $trim)) && ctype_alpha($value)) {
             if ($trim) {
                 $value = trim($value);
             }
@@ -72,7 +120,17 @@ class DatabaseConfig
         return $isAlpha;
     }
 
-    private function _isNumeric($key, $intVal = true) {
+    /**
+     * Check if an element in an array is in a numeric format. Also allows the option to get the integer value of
+     * element.
+     *
+     * @param string $key  The key of the element to check value for.
+     * @param bool $intVal Get the integer value of the element if it is numeric? By default this is true.
+     *
+     * @return bool True if the value of the specified element is in a numeric format (numbers in string or integer).
+     */
+    private function _isNumeric($key, $intVal = true)
+    {
         $isNumeric = false;
         $value = &$this->_config[$key];
 
@@ -88,7 +146,11 @@ class DatabaseConfig
     }
 
     /**
-     * @return string
+     * Validate that the database name is set and is a string; then return it.
+     *
+     * @return string Database name.
+     *
+     * @throws \Quadigital\Database\Exception\DatabaseException Thrown if database name isn't set or isn't a string.
      */
     public function getDatabase()
     {
@@ -96,11 +158,16 @@ class DatabaseConfig
             return $this->_config['database'];
         }
 
+        // Something is wrong; database name isn't set or isn't a string. Throw Database Exception.
         throw new DatabaseException(ERROR_E00004);
     }
 
     /**
-     * @return string
+     * Validate that the database host is set and is a string; then return it.
+     *
+     * @return string Database host.
+     *
+     * @throws \Quadigital\Database\Exception\DatabaseException Thrown if host isn't set or isn't a string.
      */
     public function getHost()
     {
@@ -108,11 +175,14 @@ class DatabaseConfig
             return $this->_config['host'];
         }
 
+        // Something is wrong; database host isn't set or isn't a string. Throw Database Exception.
         throw new DatabaseException(ERROR_E00004);
     }
 
     /**
-     * @return array
+     * Get the array of PDO driver options. If the array of options set isn't an array then an empty array is returned.
+     *
+     * @return array Array of PDO driver options.
      */
     public function getOptions()
     {
@@ -124,7 +194,10 @@ class DatabaseConfig
     }
 
     /**
-     * @return string
+     * Validate that the database password is set and is a string; then return it. If it's not set or isn't a valid
+     * string then an empty string is returned.
+     *
+     * @return string Database password.
      */
     public function getPassword()
     {
@@ -148,7 +221,10 @@ class DatabaseConfig
     }
 
     /**
-     * @return string
+     * Validate that the database username is set and is a string; then return it. If it's not set or isn't a valid
+     * string then an empty string is returned.
+     *
+     * @return string Database username.
      */
     public function getUsername()
     {
@@ -160,7 +236,12 @@ class DatabaseConfig
     }
 
     /**
-     * @return mixed
+     * Validate that the RDBMS (Relational database management system) is set and is a string; then return it.
+     *
+     * @return string The RDBMS.
+     *
+     * @throws \Quadigital\Database\Exception\DatabaseException Thrown if RDBMS isn't set or isn't an alphabetical
+     * string.
      */
     public function getRdbms()
     {
@@ -168,6 +249,7 @@ class DatabaseConfig
             return $this->_config['rdbms'];
         }
 
+        // Something is wrong; RDBMS isn't set or isn't an alphabetical string. Throw Database Exception.
         throw new DatabaseException(ERROR_E00003);
     }
 }
